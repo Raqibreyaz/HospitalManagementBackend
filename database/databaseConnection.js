0c36c6a447a374ba6f7cb1a70da20530bcf6bb45
RaquibReyaz <127662092+Raqibreyaz@users.noreply.github.com>
2024-05-16 17:32:21 +0530

    Updated databaseConnection

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-export const dbConnection = () => {
-    mongoose.connect(`${process.env.MONGODB_URI}/HospitalManagement`)
-        .then((res) => {
-            console.log(`mongodb connected!!`);
-        }
-        )
-        .catch((error) => {
-            console.log(`error while connecting to database ${error}`);
-        }
-        )
-}
+export const dbConnection = async () => {
 
+    try {
+
+        let connection = await mongoose.connect(`${process.env.MONGODB_URI}/HospitalManagement`)
+
+        console.log('mongodb connected!! ', connection);
+    } catch (error) {
+        console.log(`error while connecting to database ${error}`);
+    }
+}
